8a51a9af07c3388c8d956fe597d2d57f3048df7c
rzvr <work.folder.d@protonmail.com>
2022-01-05 04:06:12 +0200

    Updated scss basic styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
-import './styles/sass/App.scss';
+import "./styles/sass/components/App.scss";
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
+        Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You’ve nailed
+        the design and the responsiveness at various breakpoints works really well.
+      </div>
     </div>
   );
 }
